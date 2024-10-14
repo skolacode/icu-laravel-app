cb060acdaf9bfa68d2c366dfeef9768a88bd27c0
Mohamed Burhan <bern2727@gmail.com>
2024-10-15 02:15:21 +0800

    1. Use env values 2. update the routes

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Named Route
+Route::get('/user/profile', function () {
+    return 'Pengguna Profiles';
+})->name('user.profile');
+
 // Route Param
 Route::get('/user/{name}', function ($name) {
     return 'User '.$name;
 });
 
-// Named Route
-Route::get('/user/profile', function () {
-    return 'User Profile';
-})->name('user.profile');
+
+// alias of a route user.profile = /pengguna/profile
 
 // Redirect route to named route
 Route::get('/redirect-to-profile', function () {
@@ -23,23 +26,29 @@ Route::get('/redirect-to-profile', function () {
 
 // Route Group
 Route::prefix('food')->group(function () {
-    Route::get('{name}', function ($name) {
-        return 'Food name: '.$name;
-    });
-    Route::get('details', function () {
+
+    Route::get('/details', function () {
         return 'Food details are following';
     });
+
+    Route::get('/home', function () {
+        return 'Food home page';
+    });
+
 });
 
 
 // combination of all above (Route, Named Route, Route Param, Route Group, Route Prefix)
-Route::name('.job')->prefix('job')->group(function () {
-    Route::get('{name}', function ($name) {
-        return 'Job name: '.$name;
-    })->name('.name');
-    Route::get('description', function () {
-        return 'Job description are following';
+Route::name('job')->prefix('job')->group(function () {
+    
+    Route::get('home', function () {
+        return 'Job home page';
+    })->name('.home');
+    
+    Route::get('details', function () {
+        return 'Job details are following';
     })->name('.description');
+
 });
 
 // Custom Fall back route
