d6003ea0760f8fa1246fbed0b0cf9f3008f79bb5
Mohamed Burhan <bern2727@gmail.com>
2024-10-17 06:20:05 +0800

    1. added a auth middleware

--- routes/feed/web.php
+++ routes/feed/web.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FeedController;
 
-Route::get('/feeds', [FeedController::class, 'index'])->name('feeds');
+Route::middleware('auth')->group(function () {
+  Route::get('/feeds', [FeedController::class, 'index'])->name('feeds');
 
-Route::get('/feed/create', [FeedController::class, 'create'])->name('feed.create');
-Route::post('/feed/store', [FeedController::class, 'store'])->name('feed.store');
-
-Route::get('/feed/show/{feed}', [FeedController::class, 'show'])->name('feed.show');
-Route::put('/feed/update/{feed}', [FeedController::class, 'update'])->name('feed.update');
-
-// STEPS
-// VIEW > Controller > Route
+  Route::get('/feed/create', [FeedController::class, 'create'])->name('feed.create');
+  Route::post('/feed/store', [FeedController::class, 'store'])->name('feed.store');
+  
+  Route::get('/feed/show/{feed}', [FeedController::class, 'show'])->name('feed.show');
+  Route::put('/feed/update/{feed}', [FeedController::class, 'update'])->name('feed.update');
+});
