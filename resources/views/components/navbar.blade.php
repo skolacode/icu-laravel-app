af0ea9eb6c0cac0fa8039392f498380cd65aa9f4
Mohamed Burhan <bern2727@gmail.com>
2025-01-25 18:17:02 +0800

    feat: update the navbar
    1. Add feeds button at navbar
    2. remove unused code
    3. fix the Navbar brand name and href

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="{{ route('home') }}">ICU Feeds</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,12 +9,14 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
         </li>
+        @auth
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('feeds') }}">Feeds</a>
+          </li>
+        @endauth
         <li class="nav-item">
           <a class="nav-link" href="{{ route('about') }}">About</a>
         </li>
-        {{-- <li class="nav-item">
-          <a class="nav-link" href="{{ route('auth.signout') }}">Sign Out</a>
-        </li> --}}
       </ul>
 
       {{-- Sign out button at the far right --}}
